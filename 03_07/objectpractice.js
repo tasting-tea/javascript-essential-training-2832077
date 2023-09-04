b004cb3ad40bcd55085d51ac2add5f918b395356
tasting-tea <oncetastingtea@gmail.com>
2023-09-04 16:00:14 +0100

    New lessons

---
+++ 03_07/objectpractice.js
@@ -0,0 +1,18 @@
+/**
+ * Practice creating a nested object with a method
+ */
+
+const question = {
+  mainText:
+    "[Name] is in life situation [B/C/D]. All [their] life, [Name] has had a chronic health condition which [Name] was born with, which restricts [their] job and lifestyle choices somewhat.",
+  Character: {
+    characterName: "Ioan",
+    characterGender: "Male",
+  },
+};
+
+const cup = {
+  colour: "White",
+  volume: "120",
+  brand: "Aloha",
+};
